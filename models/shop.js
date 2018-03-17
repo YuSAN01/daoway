8f60d8ac0dc3016d74305ae541d2de16d85862e1
unknown <you@example.com>
2018-03-17 09:16:14 +0800

    'bb'

---
+++ models/shop.js
@@ -0,0 +1,8 @@
+const  mongoose = require('mongoose')
+const shopSchema = new mongoose.Schema({
+    serviceIndex:{type:String},
+    serviceType:{type:Array},
+    shopList:{type:Array}
+
+})
+module.exports = mongoose.model('shops',shopSchema)
